7c38b15dcd491ffb48dcebb550b534de547268b6
Maciek <ubernam@gmail.com>
2018-11-13 17:49:24 +0100

    Work in progress1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ class App extends Component {
 
   state = {username: 'supermax'};
 
+  InputComponent = (props) => {
+    let displayInputLength = (<p>Input length is 0.</p>);
+
+    if (props.change !== undefined){
+      displayInputLength = (<p>Input length is {props.change.length}.</p>);
+    }
+
+    return (
+      <div>
+        <input type="text" onChange={props.change}/>
+        {displayInputLength}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
-
+        
       </div>
     )
   }
