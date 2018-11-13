a4d6b2f4ff42875ffce7cfa4f7206a22eab676ca
Maciek <ubernam@gmail.com>
2018-11-13 17:23:43 +0100

    Clean before exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
 
   state = {username: 'supermax'};
 
-  changeHandler = (event) =>{
-      this.setState({username: event.target.value});
-  }
-
   render() {
     return (
       <div className="App">
-        <UserInput changed={this.changeHandler.bind(this,)} currentName={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={'Dzień dobry'}/>
+
       </div>
     )
   }
